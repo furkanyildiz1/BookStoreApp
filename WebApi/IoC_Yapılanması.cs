0fec3b9d0d66c1bcfe8799c00691dcf83ff4bba9
Furkan yıldız <yildizfurkan061@gmail.com>
2025-08-12 17:41:48 +0300

    bağlantı dizesi eklemeve ıoc yapılanma

---
+++ WebApi/IoC_Yapılanması.cs
@@ -0,0 +1,19 @@
+﻿using WebApi.Services;
+
+namespace WebApi
+{
+    public class IoC_Yapılanması
+    {
+        public IoC_Yapılanması()
+        {
+            IServiceCollection services= new ServiceCollection();
+            services.Add(new ServiceDescriptor(typeof(ConsoleLog),new ConsoleLog()));
+            services.Add(new ServiceDescriptor(typeof(TextLog), new TextLog()));
+
+            ServiceProvider provider=services.BuildServiceProvider();
+            provider.GetService<ConsoleLog>();
+            provider.GetService<TextLog>();
+
+        }
+    }
+}
