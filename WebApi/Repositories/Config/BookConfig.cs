054525245bfc187f3a2c6b3dd1abc224bffa76e5
Furkan yıldız <yildizfurkan061@gmail.com>
2025-08-13 17:49:20 +0300

    tip configürasyonları yapıldı

---
+++ WebApi/Repositories/Config/BookConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Models;
+
+namespace WebApi.Repositories.Config
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+                new Book { Id = 1, Title = "The Great Gatsby", Price = 150 },
+                new Book { Id = 2, Title = "The Great ", Price = 102 },
+                new Book { Id = 3, Title = "The", Price = 105 }
+            );
+        }
+    }
+}
